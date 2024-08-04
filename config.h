46e9a0d34a986538b60e74f4dcb8acb1caa18e1d
Pyczek <8tomapapa2137@wp.pl>
2024-08-04 21:45:09 +0200

    added custom colors, custom font and a config file

---
+++ config.h
@@ -0,0 +1,29 @@
+// Opacity level
+double opacity = 1.0;
+
+// Background 
+#define CLR_BACKGROUND "#000000"
+
+// Foreground 
+#define CLR_FOREGROUND "#ffffff"
+
+// Colors
+#define CLR_BLACK "#000000"
+#define CLR_RED "#b21818"
+#define CLR_GREEN "#18b218"
+#define CLR_YELLOW "#b26818"
+#define CLR_BLUE "#1818b2"
+#define CLR_MAGENTA "#b218b2"
+#define CLR_CYAN "#18b2b2"
+#define CLR_WHITE "#b2b2b2"
+#define CLR_LIGHT_BLACK "#686868"
+#define CLR_LIGHT_RED "#ff5454"
+#define CLR_LIGHT_GREEN "#54ff54"
+#define CLR_LIGHT_YELLOW "#ffff54"
+#define CLR_LIGHT_BLUE "#5454ff"
+#define CLR_LIGHT_MAGENTA "#ff54ff"
+#define CLR_LIGHT_CYAN "#54ffff"
+#define CLR_LIGHT_WHITE "#ffffff"
+
+// Font {font name} {font size}
+#define FONT "monospace 12"
--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <vte/vte.h>
 #include <gtk/gtk.h>
 
+#include "config.h"
+
 #define OPACITY_STEP 0.05
 #define MIN_OPACITY 0.1
 #define MAX_OPACITY 1.0
 
 GtkWidget *window;
-double opacity = 0.8;
 
 static gboolean
 on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer user_data)
@@ -41,6 +42,46 @@ void set_terminal_text_color(VteTerminal *terminal, const char *color)
     }
 }
 
+static const GdkRGBA
+sec_parse_color(const char *format)
+{
+	GdkRGBA color;
+	if (!gdk_rgba_parse(&color, format)) {
+		g_warning("Unable to parse color: %s", format);
+		exit(EXIT_FAILURE);
+	}
+
+	return (const GdkRGBA) color;
+}
+
+static void
+set_terminal_palette(VteTerminal *self)
+{
+	const GdkRGBA bg_color = sec_parse_color(CLR_BACKGROUND);
+	const GdkRGBA fg_color = sec_parse_color(CLR_FOREGROUND);
+
+	const GdkRGBA palette[] = {
+		sec_parse_color(CLR_BLACK),
+		sec_parse_color(CLR_RED),
+		sec_parse_color(CLR_GREEN),
+		sec_parse_color(CLR_YELLOW),
+		sec_parse_color(CLR_BLUE),
+		sec_parse_color(CLR_MAGENTA),
+		sec_parse_color(CLR_CYAN),
+		sec_parse_color(CLR_WHITE),
+		sec_parse_color(CLR_LIGHT_BLACK),
+		sec_parse_color(CLR_LIGHT_RED),
+		sec_parse_color(CLR_LIGHT_GREEN),
+		sec_parse_color(CLR_LIGHT_YELLOW),
+		sec_parse_color(CLR_LIGHT_BLUE),
+		sec_parse_color(CLR_LIGHT_MAGENTA),
+		sec_parse_color(CLR_LIGHT_CYAN),
+		sec_parse_color(CLR_LIGHT_WHITE)
+	};
+
+	vte_terminal_set_colors(self, &fg_color, &bg_color, palette, 16);
+}
+
 static void
 child_ready(VteTerminal *terminal, GPid pid, GError *error, gpointer user_data)
 {
@@ -68,6 +109,7 @@ int main(int argc, char *argv[])
     gtk_widget_set_opacity(GTK_WIDGET(window), opacity);
 
     gchar *command[] = {"/bin/sh", "-c", "$SHELL", NULL};
+    // gchar *command[] = {"/bin/sh", "-c", "PS1='anterminal > ' /bin/sh", NULL};
 
     vte_terminal_spawn_async(VTE_TERMINAL(terminal),
     VTE_PTY_DEFAULT,
@@ -85,6 +127,9 @@ int main(int argc, char *argv[])
     g_signal_connect(window, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(terminal, "child-exited", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(window, "key-press-event", G_CALLBACK(on_key_press), NULL);
+    // set_terminal_text_color(VTE_TERMINAL(terminal), "#ffffff");
+    set_terminal_palette(VTE_TERMINAL(terminal));
+    vte_terminal_set_font(VTE_TERMINAL(terminal), pango_font_description_from_string(FONT));
 
     vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     gtk_box_pack_start(GTK_BOX(vbox), terminal, TRUE, TRUE, 0);
